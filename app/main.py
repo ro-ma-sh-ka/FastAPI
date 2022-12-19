33fc6f1659e2b79ecfb2f122b6e04cdbbad69a58
romashka <roma.l@mail.ru>
2022-12-19 19:59:36 +0400

    sqlalchemy connected

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -6,23 +6,13 @@ from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-my_posts = [{'title': 'title1', 'content': 'content1', 'id': 1},
-            {'title': 'title2', 'content': 'content2', 'id': 2}]
-
-
-def find_post(id):
-    for post in my_posts:
-        if post['id'] == id:
-            return post
-
-
-def find_index_post(id):
-    for index, post in enumerate(my_posts):
-        if post['id'] == id:
-            return index
 
 
 class Post(BaseModel):
@@ -53,6 +43,11 @@ def get_posts():
     cursor.execute("""SELECT * FROM posts""")
     posts = cursor.fetchall()
     return {"data": posts}
+
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    return {"status": "ok"}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
