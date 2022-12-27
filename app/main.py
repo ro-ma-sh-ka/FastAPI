25eec8e9c66c9d647b504cf5a6abb64d6e47327e
romashka <roma.l@mail.ru>
2022-12-27 10:20:41 +0400

    CORS policy added

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # from . import models
 # from .database import engine
 from .routers import post, user, auth, vote
@@ -8,6 +9,18 @@ from .routers import post, user, auth, vote
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+
+# we give possibility to send requests to our API from no-localhost domains. Example: 'https://www.google.com'
+origins = ['*']
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # we use include_router to include path operations from different files after divided them
 app.include_router(post.router)
